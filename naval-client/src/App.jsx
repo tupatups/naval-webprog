8c5497894cec51b0d527c3bb795411721c535e5a
tupatups <navalchristopherbago@gmail.com>
2026-03-11 13:17:08 +0800

    lab-act1

--- naval-client/src/App.jsx
+++ naval-client/src/App.jsx
@@ -9,25 +9,17 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <h1>My React App</h1>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Name: Christopher Naval <br />
+          Email: navalchristopherbago@gmail.com <br />
+          Section: INF-231 <br />
         </p>
+        <a href="https://github.com/tupatups/naval-webprog" target="_blank">
+        Github Link
+        </a>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    
     </>
   )
 }
